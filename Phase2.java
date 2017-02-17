ddf58139fc49f19d0bcf03849e3b2c32d00e366b
April49 <april9149@gmail.com>
2017-02-16 20:43:45 -0600

    Update Phase2.java

--- Phase2.java
+++ Phase2.java
@@ -1,4 +1,9 @@
-package edu.uiowa.cs;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instruciton;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -7,23 +12,63 @@ import java.util.Map;
 
 public class Phase2 {
 
-    /* Returns a list of copies of the Instructions with the
-     * immediate field of the instruction filled in
-     * with the address calculated from the branch_label.
-     *
-     * The instruction should not be changed if it is not a branch instruction.
-     *
-     * unresolved: list of instructions without resolved addresses
-     * first_pc: address where the first instruction will eventually be placed in memory
-     */
-    int instructionaddress=0;
-    HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
     public static List<Instruction> resolve_addresses(List<Instruction> unresolved, int first_pc) {
-        instructionaddress=first_pc;
-        for(int i=0; i<unresolved.size()){
-            hmap.put(unresolved.get(i).instruction_id,instructionaddress+4(i-1));
+        
+        LinkedList<Instruction> resolved_tals = new LinkedList<Instruction>();
+        Map map_address = new HashMap(); //map of address-order (of address as positive int)
+        Map map_labelID = new HashMap(); //map of labelID-address
+        
+        //make Maps
+        for (int i=0; i<unresolved.size();i++){
+            //address-order map
+            map_address.put(first_pc, i);
+
+            //labelID-address map
+            if(unresolved.get(i).branch_label!=0) {
+                map_labelID.put(unresolved.get(i).copy().branch_label, first_pc);
+            }
+            
+            first_pc = first_pc+4; 
         }
-        return unresolved;
+        
+        
+        for (int j=0; j<unresolved.size(); j++){
+            Instruction unresolved_tals_tmp = unresolved.get(j);
+            
+            //branch_label is 0
+            if(unresolved_tals_tmp.branch_label==0){
+                resolved_tals.add(unresolved_tals_tmp.copy());
+            }
+            
+            //branch_label is not 0
+            else {
+                //target address
+                int target_address = (int) map_labelID.get(unresolved_tals_tmp.branch_label);
+                int target_address_order = (int) map_address.get(target_address);
+                
+                //modifiy immediate
+                //j is current address
+                if(target_address_order<j){
+                    int immediate = target_address_order-j-1;
+                    
+                    //modify immediate and branchLabel
+                    //store in resolved_tals
+                    unresolved_tals_tmp.immediate = immediate;
+                    unresolved_tals_tmp.branch_label = 0;
+                    resolved_tals.add(unresolved_tals_tmp.copy());
+                }
+                
+                if(target_address_order>j){
+                    int immediate = target_address_order-j-1;
+                    
+                    unresolved_tals_tmp.immediate = immediate;
+                    unresolved_tals_tmp.branch_label = 0;
+                    resolved_tals.add(unresolved_tals_tmp.copy());
+                }   
+            }
+        }
+        return resolved_tals;
     }
 
 }
+
