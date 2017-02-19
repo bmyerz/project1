3622ec0499733fd8cfdeb6c6a1ed3e274a711995
hyo kyung kang <hyokyung-kang@uiowa.edu>
2017-02-19 00:02:49 -0600

    Update Phase3.java

--- Phase3.java
+++ Phase3.java
@@ -4,11 +4,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package instruciton;
+package edu.uiowa.cs;
 
 /**
  *
- * @author gyeonghyeko, hyokyung kang
+ * @author hyokyung kang
  */
 import java.util.LinkedList;
 import java.util.*;
@@ -53,14 +53,9 @@ public class Phase3 {
     }
         return stack;
     }
+    
 
-     public static void Stack<Integer> showpop(Stack st) {
-      System.out.print("pop -> ");
-      Integer a = (Integer) st.pop();
-      System.out.println(a);
-      System.out.println("stack: " + st);
-    }
-
+     
       
 
     public static List<Integer> translate_instructions(List<Instruction> tals) {
@@ -88,29 +83,30 @@ public class Phase3 {
                 functnumber=42;
                 }
                 
-                for(int i=0;i<decimaltobinary6(0).size();){
+                for(int i=0;i<decimaltobinary6(0).size()){
                  int q=decimaltobinary6(0).pop();
                     Binary_list.add(q);
                     
                 }
-                for(int i=0;i<decimaltobinary5(tals_tmp.rs).size()){
+                for(int i=0;i<decimaltobinary5(tals_tmp.rs).size();i++){
                 int q=decimaltobinary5(tals_tmp.rs);
                  Binary_list.add(q);
                
                 
                 }
-                for(int i=0;i<decimaltobinary5(tals_tmp.rt).size();){
+                for(int i=0;i<decimaltobinary5(tals_tmp.rt).size();i++){
                 int q=decimaltobinary5(tals_tmp.rt).pop();
                  Binary_list.add.(q);            
                 }
-                for(int i=0;i<decimaltobinary5(tals_tmp.rd).size();){
+                for(int i=0;i<decimaltobinary5(tals_tmp.rd).size();i++){
                 int q=decimaltobinary5(tals_tmp.rd).pop();
                  Binary_list.add.(q);            
                 }
-                for(int i=0;i<decimaltobinary5(functnumber).size();){
+                for(int i=0;i<decimaltobinary5(functnumber).size();i++){
                 int q=decimaltobinary5(functnumber).pop();
                  Binary_list.add.(q);            
                 }
+            }
                                            
                 
                 
@@ -129,79 +125,32 @@ public class Phase3 {
                 if(tals_tmp.instruction_id==10){
                 opcode=13;}
                 
-                
-                
+              for(int i=0;i<decimaltobinary6(opcode).size();i++){
+                 int q=decimaltobinary6(opcode).pop();
+                    Binary_list.add(q);
                     
-                
-                
-            }          
-                
-            //addiu(I)
-            /*if (tals_tmp.instruction_id==1){//addiu_I_type
-                String addiu_op_binary = Integer.toBinaryString(9);
-                String addiu_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String addiu_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String addiu_immediate_binary = Integer.toBinaryString(tals_tmp.immediate);
-                
-                String addiu_string_binary = addiu_op_binary + addiu_rs_binary + addiu_rt_binary +addiu_immediate_binary;
-                int addiu_binary = Integer.valueOf(addiu_string_binary);
-                binary_list.add(addiu_binary);
-            }*/
-            
-            
-            
-            //beq(I)
-            /*if (tals_tmp.instruction_id==5){
-                String beq_op_binary = Integer.toBinaryString(4);
-                String beq_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String beq_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String beq_immediate_binary = Integer.toBinaryString(tals_tmp.immediate);
-                
-                String addiu_string_binary = beq_op_binary + beq_rs_binary + beq_rt_binary +beq_immediate_binary;
-                int addiu_binary = Integer.valueOf(addiu_string_binary);
-                binary_list.add(addiu_binary);
-            }*/
-            
-            //bne(I)
-           /* if (tals_tmp.instruction_id==6){
-                String bne_op_binary = Integer.toBinaryString(5);
-                String bne_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String bne_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String bne_immediate_binary = Integer.toBinaryString(tals_tmp.immediate);
-                
-                String addiu_string_binary = bne_op_binary + bne_rs_binary + bne_rt_binary +bne_immediate_binary;
-                int addiu_binary = Integer.valueOf(addiu_string_binary);
-                binary_list.add(addiu_binary);
-            }*/
-            
-           
-            /*if(tals.tmp.instruction_id==9){//lui // I-type
+                }
+               for(int i=0;i<decimaltobinary5(rs).size();i++){
+                 int q=decimaltobinary5(rs).pop();
+                    Binary_list.add(q);
                     
-                String lui_op_binary = Integer.toBinaryString(15);
-                String lui_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String lui_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String lui_immediate_binary = Integer.toBinaryString(tals_tmp.immediate);
-                
-                String lui_string_binary = lui_op_binary + lui_rs_binary + lui_rt_binary +lui_immediate_binary;
-                int lui_binary = Integer.valueOf(lui_string_binary);
-                binary_list.add(lui_binary);
-
-            }
-            if(tals.tmp.instruction_id==10){//ori //I-type
+                }
+               
+               for(int i=0;i<decimaltobinary5(rt).size();i++){
+                 int q=decimaltobinary5(rt).pop();
+                    Binary_list.add(q);
                     
-               String ori_op_binary = Integer.toBinaryString(13);
-               String ori_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-               String ori_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-               String ori_immediate_binary = Integer.toBinaryString(tals_tmp.immediate);
-               String ori_string_binary = ori_op_binary + ori_rs_binary + ori_rt_binary +ori_immediate_binary;
-               int ori_binary = Integer.valueOf(ori_string_binary);
-               binary_list.add(ori_binary);
-
-
-            }*/
-            
-           
-        }
-        return null;
+                }
+               
+             
+                if(tals_tmp.immediate)<=0){  
+                tals_tmp.immediate=tals_tmp.immediate*(-1);
+                int b = Integer.parseInt("tals_tmp.immediate",2);
+                int a = Integer.parseInt("(2^12-1)",2);             
+                int c=a|b;
+                  }
+                Binary_list.add(c);}          
+               }
+        return binary_list;
     }
 }
