44263f8f157eb914807cfcd8395e5173f6f651ad
hyo kyung kang <hyokyung-kang@uiowa.edu>
2017-02-18 23:06:28 -0600

    Update Phase3.java

--- Phase3.java
+++ Phase3.java
@@ -24,25 +24,45 @@ public class Phase3 {
      *
      */
     int functnumber;
+    int opcode;
     
-    public static void decimaltobinary5(Stack st, int a) {// change integer to binary number put those numbers in stack.. 
+    public static Stack<Integer> decimaltobinary5(int a) {// change integer to binary number put those numbers in stack.. 
+        Stack<Integer> stack = new Stack<Integer>();
         while (a != 0)
         {
         int d = a % 2;
-        st.push(d);
+        stack.push(d);
         a /= 2;
         }
         while(st.size()<=5){
-         st.push(0)}}
-    public static void decimaltobinary6(Stack st, int a) {// change integer to binary number put those numbers in stack.. 
+         stack.push(0)}
+   
+        }
+        return stack;
+    }
+    public static Stack<Integer> decimaltobinary6(int a) {// change integer to binary number put those numbers in stack.. 
+        Stack<Integer> stack = new Stack<Integer>();
         while (a != 0)
         {
         int d = a % 2;
-        st.push(d);
+        stack.push(d);
         a /= 2;
         }
         while(st.size()<=6){
-         st.push(0)}}
+         stack.push(0)}
+    }
+        return stack;
+    }
+
+     public static void Stack<Integer> showpop(Stack st) {
+      System.out.print("pop -> ");
+      Integer a = (Integer) st.pop();
+      System.out.println(a);
+      System.out.println("stack: " + st);
+    }
+
+      
+
     public static List<Integer> translate_instructions(List<Instruction> tals) {
         
         List<Integer> binary_list = new LinkedList<Integer>();
@@ -56,6 +76,7 @@ public class Phase3 {
             
             
             if(tals_tmp.instruction_id==2||tals_tmp.instruction_id==3||tals_tmp.instruction_id==8){
+                // check the Id is R-type
                 
                 if(tals_tmp.instruction_id==2){
                 functnumber=33;
@@ -67,11 +88,20 @@ public class Phase3 {
                 functnumber=42;
                 }
                 
+                for(i=0;i<decimaltobinary6(0).size();){
+                Binary_list.add.decimaltobinary6(0).pop();
+                    
+                }
+             decimaltobinary6(st, 0); decimaltobinary5(st,tals_tmp.rs);decimaltobinary5(st,tals_tmp.rt);decimaltobinary5(st,tals_tmp.rd);decimaltobinary5(st,tals_tmp.shift);decimaltobinary6(st, 42);
+                
+               
                 /*
                 change integer to binary 
                  String x=Integer.toBinaryString()
                  int y=Integer.parseInt(x)
                 */
+                
+                //op//rs//rt//rd//shift//funct
                 
                 
                 
@@ -82,50 +112,24 @@ public class Phase3 {
             if(tals_tmp.instruction_id==1||tals_tmp.instruction_id==5||tals_tmp.instruction_id==6||tals_tmp.instruction_id==9||tals_tmp.instruction_id==10){
             // put the algorithm for only I-type
             //need op,rs,rt,immediate//
-            }
-            
-                 /*
-            if (){//addu rtype 
-                String addu_op_binary = Integer.toBinaryString(0);
-                String addu_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String addu_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String addu_rd_binary = Integer.toBinaryString(tals_tmp.rd);
-                String addu_shift_binary = Integer.toBinaryString(tals_tmp.shift_amount);
-                String addu_funct_binary = Integer.toBinaryString(33);
+                if(tals_tmp.instruction_id==1){
+                opcode=42;}
+                if(tals_tmp.instruction_id==5){
+                opcode=4;}
+                if(tals_tmp.instruction_id==6){
+                opcode=5;}
+                if(tals_tmp.instruction_id==9){
+                opcode=15;}
+                if(tals_tmp.instruction_id==10){
+                opcode=13;}
                 
-                String addu_string_binary = addu_op_binary + addu_rs_binary + addu_rt_binary +addu_rd_binary + addu_shift_binary + addu_funct_binary;
-                int addu_binary = Integer.valueOf(addu_string_binary);
-                binary_list.add(addu_binary);
-            }
-           
-            //or(R)
-            if (tals_tmp.instruction_id==3){
-                String or_op_binary = Integer.toBinaryString(0);
-                String or_rs_binary = Integer.toBinaryString(tals_tmp.rs);
-                String or_rt_binary = Integer.toBinaryString(tals_tmp.rt);
-                String or_rd_binary = Integer.toBinaryString(tals_tmp.rd);
-                String or_shift_binary = Integer.toBinaryString(tals_tmp.shift_amount);
-                String or_funct_binary = Integer.toBinaryString(37);
                 
-                String or_string_binary = or_op_binary + or_rs_binary + or_rt_binary +or_rd_binary + or_shift_binary + or_funct_binary;
-                int or_binary = Integer.valueOf(or_string_binary);
-                binary_list.add(or_binary);
-            }
-            if(tals.tmp.instruction_id==8){//slt//R-type
-               String slt_op_binary = Integer.toBinaryString(0);
-               String slt_rs_binary=Integer.toBinaryString(tals_tmp.rs);
-               String slt_rt_binary=Integer.toBinaryString(tals_tmp.rt);
-               String slt_rd_binary=Integer.toBinaryString(tals_tmp.rd);
-               String slt_shamt_binary=Integer.toBinaryString(tals_tmp.shift_amount);
-               String slt_funct_binary = Integer.toBinaryString(42);
-               String slt_string_binary = slt_op_binary +  slt_rs_binary +  slt_rt_binary +slt_rd_binary+slt_shamt_binary+slt_funct_binary;
-               int slt_binary = Integer.valueOf(slt_string_binary);
-               binary_list.add(slt_string_binary);
                 
-
-
-            }*/
-            
+                    
+                
+                
+            }          
+                
             //addiu(I)
             /*if (tals_tmp.instruction_id==1){//addiu_I_type
                 String addiu_op_binary = Integer.toBinaryString(9);
